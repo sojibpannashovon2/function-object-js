62e0df1429812834c668a001d5ed99d1b8996222
Md Arifur Rahman Sojib <sojibkhanarif999@gmail.com>
2022-08-06 13:03:49 +0600

    function-parameter

---
+++ function.js
@@ -0,0 +1,16 @@
+
+
+// Declear a funtion
+
+function startFunction() {
+
+    console.log(" How are you man");
+    console.log(" I am fine ");
+    console.log(" What are you doing babe");
+}
+
+// Call a function
+
+startFunction();
+
+console.log("tui kemon achis re", "Ami valo nei");
---
+++ parameter.js
@@ -0,0 +1,10 @@
+
+function bringCha(when) {
+
+    console.log("Apu cha de amare");
+    console.log(when);
+}
+
+// Call the function
+
+bringCha("6:30");
